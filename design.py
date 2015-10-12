d428cced5e66c18e9a0ee7ae91e7bb15e4905dc6
William Ehlhardt <williamehlhardt@gmail.com>
2015-10-12 16:50:44 -0700

    Refer to paths as "placements" instead.
    The logic here is that a placement is a list of tiles with associated machine-bit
    markings. Later, we'll probably want to retain actual machine coordinates instead
    of just their marked-tile sets.

--- design.py
+++ design.py
@@ -4,32 +4,34 @@ ROTATIONS = [0, 90, 180, 270]
 
 def search_internal(
         board, machine_list, goal,
-        path_so_far, position, next_machine_index):
+        placements, position, next_machine_index):
 
     # Have we placed all the machines?
     if next_machine_index == len(machine_list):
         # Check whether we've reached the goal.
         if goal == position:
-            yield path_so_far
+            yield placements
         return
 
     # Attempt to place the next machine at the current position.
     for angle in ROTATIONS:
-        path_extn, new_position = place_machine(
+        placement_extn, new_position = place_machine(
             position, angle, machine_list[next_machine_index])
 
         # Do any of the newly placed machine tiles overlap something
         # else placed so far?
-        if path_collides(path_so_far, path_extn):
+        if placement_collides(placements, placement_extn):
             continue
 
         # What about hitting wall tiles?
-        if path_hits_wall(path_extn, board):
+        if placement_hits_wall(placement_extn, board):
             continue
+
+        new_placement = placements + placement_extn
 
         succ = search_internal(
             board, machine_list, goal,
-            new_path, new_position, next_machine_index + 1)
+            new_placement, new_position, next_machine_index + 1)
         for soln in succ:
             yield soln
 
