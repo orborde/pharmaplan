1fcd26abd0144900a7ebeaddb606d748cb348213
William Ehlhardt <williamehlhardt@gmail.com>
2015-10-14 01:10:35 -0700

    Machine specifications

--- design.py
+++ design.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python
 
+import collections
+
 ROTATIONS = set([0, 90, 180, 270])
 
 # TODO: This is a group-theoretic correct way of doing this, but
@@ -20,6 +22,83 @@ def rotate(row, col, angle):
         angle -= 90
         row, col = col, -row
     return row, col
+
+
+# MACHINE SPECIFICATIONS
+#
+# A machine may be either a process step OR a belt tile. Each machine
+# is represented as a potential "move" through the space of the
+# factory floor. This "move" has two parts:
+#
+# - A set of tiles that will be marked as occupied, either by the
+#   machine itself or by a belt tile, and
+# - A destination to which the cursor will move to place the next machine.
+
+Machine = collections.namedtuple('Machine',
+                                 ['name', 'marks', 'destination'])
+
+# A machine's input tile (the tile from which the input draws, not the
+# one containing the arrow) is always the origin of the machine's
+# geometry. The input tile should always be left unmarked by the move;
+# its marking is expected to happen as a result of the prior machine
+# placement.
+#
+# The output tile will always be marked as a belt tile. Thus, machines
+# can be chained directly if there is only one belt tile connecting
+# them (as opposed to placing "belt" machines between them).
+#
+# For example, the Dissolver's specification looks like this:
+
+DISSOLVER = Machine(
+    name = 'Dissolver',
+    marks = [(1,  0, 'D'),   # Place a D tile below current.
+             (1, -1, 'D'),   # Place a D southwest of current.
+             (0, -1, 'B')],  # Place a B(elt) west of current.
+    destination = (0, -1))   # Move 1 tile west.
+
+# A Belt is represented as another possible machine:
+
+BELT = Machine(
+    name = 'Belt',
+    marks = [(0, 1, 'B')],   # Place a B(elt) to the east.
+    destination = (0, 1))    # Move to the newly placed B.
+
+
+# More machine definitions follow...
+EVAPORATOR = Machine(
+    name = 'Evaporator',
+    marks = [(1, 0, 'E'),
+             (1, 1, 'E'),
+             (1, 2, 'B')],
+    destination = (1, 2))
+
+
+MACHINES = [DISSOLVER, BELT, EVAPORATOR]
+
+# Check for bonehead mistakes in the machine specifications.
+def machine_valid(m):
+    # Are all the mark tiles unique?
+    marks = set( (r,c) for r,c,_ in m.marks )
+    if len(marks) != len(m.marks): return False
+
+    # Does the destination get marked with a belt?
+    belt_marked = False
+    for r,c,mark in m.marks:
+        if mark == 'B':
+            if (r,c) != m.destination:
+                return False
+            belt_marked = True
+            break
+    if not belt_marked: return False
+
+    # Is the input tile clear?
+    for r,c,_ in m.marks:
+        if (r,c) == (0,0): return False
+
+    return True
+
+assert all(machine_valid(m) for m in MACHINES)
+
 
 def search_internal(
         board, machine_list, goal,
@@ -56,7 +135,8 @@ def search_internal(
 
 
 # TODO: Call search() with a machine, belt, machine, belt,
-# etc. machine_list.
+# etc. machine_list. Unless there's only one belt between each
+# machine, in which case you don't need to.
 
 if __name__ == '__main__':
     import doctest
